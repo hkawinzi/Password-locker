263f7a9a2dc8de46c9de02684264958c3dc3b600
happiness <happinesskawinzi@gmail.com>
2019-08-17 08:48:25 +0300

    creating an array

--- Quotes/src/app/app.component.ts
+++ Quotes/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  titles: string[];
+
+  constructor(){
+    this.titles = ['You must be the change you wish to see in the world — Gandhi','Everybody is a genius. But if you judge a fish by its ability to climb a tree, it will live its whole life believing that it is stupid — Albert Einstein','A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing— George Bernhard Shaw','God, grant me the serenity to accept the things I cannot change, the courage to change the things I can, and the wisdom to know the difference — Reinhold Niebuhr', 'Life is never made unbearable by circumstances, but only by lack of meaning and purpose — Viktor Frankl']
+  }
 }
