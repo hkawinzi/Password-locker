853cf5fc76af56369816032ee05b3a519308e819
happiness <happinesskawinzi@gmail.com>
2019-08-31 09:14:02 +0300

    creating a test to check if all the object are represented correctly

--- Password-Locker/test_Password.py
+++ Password-Locker/test_Password.py
@@ -3,4 +3,6 @@ from User import User
 
 class TestUser(unittest.TestCase):
     def setUp(self):
-   
+        self.new_User = User("James","Muriuki","0712345678","james@ms.com") # create contact object
+    
+    def test_init(self):
