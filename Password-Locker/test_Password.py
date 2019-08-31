b195e7594b50c084ef9c48dce859ef89a45577a9
happiness <happinesskawinzi@gmail.com>
2019-08-31 09:16:00 +0300

    changing contact object

--- Password-Locker/test_Password.py
+++ Password-Locker/test_Password.py
@@ -3,6 +3,14 @@ from User import User
 
 class TestUser(unittest.TestCase):
     def setUp(self):
-        self.new_User = User("James","Muriuki","0712345678","james@ms.com") # create contact object
+        self.new_User = User("Happy","Kawinzi","0712345678","hkawinzi@gmail.com") 
     
     def test_init(self):
+
+        self.assertEqual(self.new_User.first_name,"Happy")
+        self.assertEqual(self.new_User.last_name,"Kawinzi")
+        self.assertEqual(self.new_User.phone_number,"0712345678")
+        self.assertEqual(self.new_User.email,"hkawinzi@gmail.com")
+
+if __name__ == '__main__':
+    unittest.main()
