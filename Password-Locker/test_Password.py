18c03f1cddf5cbd15aaa48aaca2df597eb4dfc2f
happiness <happinesskawinzi@gmail.com>
2019-08-31 09:36:45 +0300

    running test and passed it round 1

--- Password-Locker/test_Password.py
+++ Password-Locker/test_Password.py
@@ -1,16 +1,16 @@
 import unittest
-from User import User 
+from Password import User 
 
 class TestUser(unittest.TestCase):
     def setUp(self):
-        self.new_User = User("Happy","Kawinzi","0712345678","hkawinzi@gmail.com") 
+        self.new_user = User("Happy","Kawinzi","0712345678","hkawinzi@gmail.com") 
     
     def test_init(self):
 
-        self.assertEqual(self.new_User.first_name,"Happy")
-        self.assertEqual(self.new_User.last_name,"Kawinzi")
-        self.assertEqual(self.new_User.phone_number,"0712345678")
-        self.assertEqual(self.new_User.email,"hkawinzi@gmail.com")
+        self.assertEqual(self.new_user.first_name,"Happy")
+        self.assertEqual(self.new_user.last_name,"Kawinzi")
+        self.assertEqual(self.new_user.phone_number,"0712345678")
+        self.assertEqual(self.new_user.email,"hkawinzi@gmail.com")
 
 if __name__ == '__main__':
     unittest.main()
