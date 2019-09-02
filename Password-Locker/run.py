28d2dc298c9c19b54b70719a6dfcbb843c51b358
happiness <happinesskawinzi@gmail.com>
2019-09-02 08:33:52 +0300

    function that takes in user object and calls the method to save the user

--- Password-Locker/run.py
+++ Password-Locker/run.py
@@ -1,2 +1,7 @@
 from Password import User
-def create_user(fname,lname,)
+def create_user(fname,lname,number,username):
+    new_user = User(fname,lname,number,username)
+    return new_user
+
+def save_user(user):
+    user.save_user()
